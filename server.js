92babc4e3d9001ef354ae5930a3e6e75751e3f09
joshinreddy <30510885+joshinreddy@users.noreply.github.com>
2017-08-05 22:50:06 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,17 +1,57 @@
-<!doctype html>
 <html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="center">
-            <img src="/ui/madi.png" class="img-medium"/>
-        </div>
-        <br>
-        <div class="center text-big bold">
-            This is my first trial on web app.
-        </div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+  <head>
+      <style>
+       <title>
+          artical one i joshin 
+     </title>
+     div.gallery {
+    margin: 5px;
+    border: 1px solid #ccc;
+    float: left;
+    width: 180px;
+}
+
+div.gallery:hover {
+    border: 1px solid #777;
+}
+
+div.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+div.desc {
+    padding: 15px;
+    text-align: center;
+}
+<div class="gallery">
+  <a target=" href="img_fjords.jpg">
+    <img src="/" alt="Trolltunga Norway" width="300" height="200">
+  </a>
+    </style>
+    <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+   <body>
+       <div class="docker">
+           <div>
+               <iframe src="/" height="200" width="400"></iframe>
+                 <a href="/">home</a>
+            </div>
+            <hr>
+            <h3>
+                Atrical one
+                </h3>
+                <div>
+                    Aug 5 2017 
+                    </div>
+            <div>
+                <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
+                    </p>
+                    <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
+                    </p>
+                    <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
+                    </p>
+            </div>
+         </div>   
+  </body>
+ </html>
