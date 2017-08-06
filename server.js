f8120f2fb9815a0ca1c0362f3390915db8698e0b
joshinreddy <30510885+joshinreddy@users.noreply.github.com>
2017-08-05 22:50:30 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,57 +1,36 @@
-<html>
-  <head>
-      <style>
-       <title>
-          artical one i joshin 
-     </title>
-     div.gallery {
-    margin: 5px;
-    border: 1px solid #ccc;
-    float: left;
-    width: 180px;
-}
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
 
-div.gallery:hover {
-    border: 1px solid #777;
-}
+var app = express();
+app.use(morgan('combined'));
 
-div.gallery img {
-    width: 100%;
-    height: auto;
-}
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/artical-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+});
+app.get('/artical-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-two.html'));
+});
+app.get('/artical-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'artical-three.html'));
+});
 
-div.desc {
-    padding: 15px;
-    text-align: center;
-}
-<div class="gallery">
-  <a target=" href="img_fjords.jpg">
-    <img src="/" alt="Trolltunga Norway" width="300" height="200">
-  </a>
-    </style>
-    <link href="/ui/style.css" rel="stylesheet" />
-  </head>
-   <body>
-       <div class="docker">
-           <div>
-               <iframe src="/" height="200" width="400"></iframe>
-                 <a href="/">home</a>
-            </div>
-            <hr>
-            <h3>
-                Atrical one
-                </h3>
-                <div>
-                    Aug 5 2017 
-                    </div>
-            <div>
-                <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
-                    </p>
-                    <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
-                    </p>
-                    <p> This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method This is my context for trial an error method
-                    </p>
-            </div>
-         </div>   
-  </body>
- </html>
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
